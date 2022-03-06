c337964fca7c0f95e1586306331fb6f57d2bca74
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-03-06 23:36:39 +0900

    JWT 2-1
    기본설정

---
+++ JWT/src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.jwt.config;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()  //JWT 는 아이디 비번 로그인 필요없다
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access(" hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+    }
+}
