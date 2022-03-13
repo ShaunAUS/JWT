5797611f2f2ea26105654f60a7f65ffbcc853eee
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2022-03-13 23:27:20 +0900

    Revert "JWT2-8"
    This reverts commit 9120f8d4531fbeb61cd4ff7c92a7e868063a2347.

--- JWT2-8/src/main/java/com/example/jwt/jwtt/JwtProperties.java
+++
@@ -1,9 +0,0 @@
-package com.example.jwt.jwtt;
-
-public interface JwtProperties {
-
-    String SECRET = "cos";
-    int EXPIRATION_TIME = 60000*10;  //10분
-    String TOKEN_PREFIX ="Bearer";
-    String HEADER_STRING = "Authorization";
-}
